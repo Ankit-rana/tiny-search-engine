8137d14162641cdbae63ea8f1c423eb21df6b8f5
Ankit Rana <ankit9.7.91@gmail.com>
2020-03-05 02:03:56 +0530

    corrected asyncio code

--- crawler.py
+++ crawler.py
@@ -11,21 +11,28 @@ async def get_link(urls):
 
 async def url_logic(url,i,depth, destination):
     urls = []
-    if i == depth or not url.startswith('http'):
+    if i == depth:
+        return
+    if not url.startswith('http'):
         return
     ## get the html page
+    print("%s step1"%url)
     data = await get_content(url)
 
     ## find out the links in the page and add it to a list
+    print("%s step2"%url)
     find_links(data, urls) if depth > 0 else sys.exit(1)
 
     ## print in the file (url,depth,content)
     global COUNT
+    print("%s step3"%url)
     COUNT = write_content(url, str(depth), data, destination, COUNT)
 
-    print(urls)
+    tasks = []
     async for u in get_link(urls):
-        await url_logic(u, i+1, depth, destination)
+        tasks.append(asyncio.create_task(url_logic(u, i+1, depth, destination)))
+    await asyncio.gather(*tasks)
+
 
 async def main():
     global COUNT
